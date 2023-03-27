e43b9ce97e6ed925be847c42cdad3292a1c947ce
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2023-03-27 20:28:49 +0800

    get message from txt

--- src/helper.java
+++ src/helper.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 
@@ -9,7 +13,7 @@ public class helper {
         Employee d=new Employee("d",200);
         Employee e=new Employee("e",100);
         ArrayList<Employee> array=new ArrayList<>();
-        ArrayList<Employee> array1=new ArrayList<>();
+        ArrayList<Employee> array1;
 
         CompareForPayRate comparator=new CompareForPayRate();
         Heap<Employee,CompareForPayRate> heap=new Heap(array,comparator);
@@ -18,9 +22,30 @@ public class helper {
         heap.insert(d);
         heap.insert(c);
         heap.insert(b);
-        array1=heap.sort();
-        for (int i=0;i<=array1.size()-1;i++)
-            System.out.println(array1.get(i).payRate);
+
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("employee.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split(",");
+                String name = fields[0];
+                double salary = Double.parseDouble(fields[1]);
+                Employee employee = new Employee(name, salary);
+                // do something with the employee object
+                heap.insert(employee);
+            }
+            array1=heap.sort();
+            for (int i=0;i<=array1.size()-1;i++)
+                System.out.println(array1.get(i).payRate);
+            reader.close();
+        } catch (
+                IOException error) {
+            error.printStackTrace();
         }
-        
     }
+
+}
+
+        
+
